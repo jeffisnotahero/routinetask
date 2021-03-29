41bc387caf884bb1a0a7ce60603df5001f1356c9
Wang Wai Siong <wangwai@oregonstate.edu>
2021-03-29 22:36:13 +0900

    Completed computation & update feature for Distributors' Net sales & Net profit margin

--- routine.py
+++ routine.py
@@ -2,7 +2,7 @@
 # Description: Automate routine task from work
 
 # <Features>
-# 1. Calculate data and display as output as below
+# 1. Compute data and display as output as below
 #     1. 3 Distributors & Other's
 #         1. Booking data
 #         2. Revenue data
@@ -18,7 +18,7 @@
 
 # Logic & pseudocode
 
-    # //Calculate 3 distributors & Other's data//
+    # //Compute 3 distributors & Other's data//
     # -Open revenue CSV and read everything into memory
     # -Open booking CSV and read everything into memory
 
@@ -39,17 +39,17 @@
     
     # -Create objects array with 3 distributor Objects
     #     -Loop through it
-    #         -Calculate Net sales data
+    #         -Compute Net sales data
     #             -Net sales data = Revenue - Cost
     #             -update distributor's Net sales data property
 
-    #         -Calculate Net profit margin
-    #             -Net profit margin = Net profit date / total_revenue
+    #         -Compute Net profit margin
+    #             -Net profit margin = Net sales data / total_revenue
     #             -Assign Net profit margin by modifying Net profit margin properties
 
     # -Print out all Booking, Revenue, Cost, Net sales, Net profit margin value for 3 distributors and Other
 
-    #  //Calculate Monthly total Booking & Revenue//
+    #  //Compute Monthly total Booking & Revenue//
 
     # -Create monthly_performance Parent Class with total_amount, cost, Net profit, Net profit margin properties
     # -Create revenue & booking Child Class
@@ -58,11 +58,11 @@
     # -Loop every revenue, cost amount row of revenue CSV
     #     -update revenue's total_amount, cost property
 
-    # -Calculate Net sales data
+    # -Compute Net sales data
     #     -Net sales data = Revenue - Cost
     #     -update revenue's Net sales data property
 
-    # -Calculate Net profit margin
+    # -Compute Net profit margin
     #     -Net profit margin = Net sales data / total_revenue
     #     -Assign Net profit margin by modifying Net profit margin properties
         
@@ -70,7 +70,7 @@
     #     -update booking's total_amount property
     
     # -Print out all monthly Booking & Revenue's total amount, cost, Net sales data & Net profit margin
-    
+
 import csv
 import sys
 
@@ -82,32 +82,64 @@ if len(sys.argv) != 2:
 
 # Create Distributor class
 class Distributor:
-    def __init__(self, booking, revenue, cost, net_sales, net_profit_margin):
+    def __init__(self, name, booking, revenue, cost, net_sales, net_profit_margin):
+        self.name = name
         self.booking = booking
         self.revenue = revenue
         self.cost = cost
         self.net_sales = net_sales
         self.net_profit_margin = net_profit_margin
 
-# Create Object for 3 distributors & Other
-m = Distributor(0, 0, 0, 0, 0)
-t = Distributor(0, 0, 0, 0, 0)
-n = Distributor(0, 0, 0, 0, 0)
-o = Distributor(0, 0, 0, 0, 0)
+    def __repr__(self):
+        return self.name
 
-# Function for update data
-def update(distributor):
+# Create Object for 3 distributors & Other
+m = Distributor("m", 0, 0, 0, 0, 0)
+t = Distributor("t", 0, 0, 0, 0, 0)
+n = Distributor("n", 0, 0, 0, 0, 0)
+o = Distributor("o", 0, 0, 0, 0, 0)
+
+# Function for updating distributors' Revenue & Cost
+def update_distributor_revenue_and_cost(distributor):
     revenue_distributor = row[12]
     distributor.revenue += int(revenue_distributor)
     cost_distributor = row[13]
     distributor.cost += int(cost_distributor)
+
+# Function for updating distributors' Net sales
+def update_distributor_net_sales(distributor, distributor_net_sales):
+    distributor.net_sales = distributor_net_sales
+
+# Function for updating distributor's Net profit margin
+def update_distributor_net_profit_margin(distributor, distributor_profit_margin):
+    distributor.net_profit_margin = distributor_profit_margin
+
+# Function for computing distributors' Net sales
+def compute_distributor_net_sales(distributor):
+    distributor_net_sales = distributor.revenue - distributor.cost
+    return distributor_net_sales
+
+# Function for computing distributors' Net profit margin
+def compute_distributor_net_profit_margin(distributor):
+
+    # Check if division by zero (aka distributor.revenue == 0)
+    if distributor.revenue == 0:
+        distributor_net_profit_margin = 0
+        return distributor_net_profit_margin
+    else:
+        distributor_net_profit_margin = distributor.net_sales / distributor.revenue
+        return distributor_net_profit_margin
+
+def format_value_with_percentage(original_value):
+    percentage_value = "{0:.2%}".format(original_value)
+    return percentage_value
 
 
 # Open revenue CSV and read everything into memory
 with open(sys.argv[1], "r", encoding='shift_jis') as database:
     revenue_data = csv.reader(database)
 
-    # Skip first row (description)
+    # Skip first row (Description row)
     next(revenue_data)
 
     # Assign values into corresponding property for each distributors
@@ -119,32 +151,63 @@ with open(sys.argv[1], "r", encoding='shift_jis') as database:
 
         # "m" distributor
         if row[0] == "10590|M":    
-            update(m)
+            update_distributor_revenue_and_cost(m)
         
         elif row[0] == "10601|T":    
-            update(t)
+            update_distributor_revenue_and_cost(t)
 
         elif row[0] == "ｴ9905|N":    
-            update(n)
+            update_distributor_revenue_and_cost(n)
         
         else:
-            update(o)
+            update_distributor_revenue_and_cost(o)
     
     total_revenue = m.revenue + t.revenue + n.revenue + o.revenue
+    total_cost = m.cost + t.cost + n.cost + o.cost
 
-    print(f"m revenue:{m.revenue}, t revenue:{t.revenue}, n revenue:{n.revenue}, o revenue:{o.revenue}, total revenue:{total_revenue}")
+    print("hello")
+    print(f"""
+    m revenue:{m.revenue}
+    t revenue:{t.revenue} 
+    n revenue:{n.revenue} 
+    o revenue:{o.revenue} 
+    total revenue:{total_revenue}
+    """)
+
+    print(f""" 
+    m cost:{m.cost}
+    t cost:{t.cost} 
+    n cost:{n.cost} 
+    o cost:{o.cost} 
+    total cost:{total_cost}
+    """)
+
+    all_distributor_list = [m, t, n, o] # List guaranteed to be iterated in order
+
+    # Compute each distributors' Net sales data & Net profit margin
+    for distributor in all_distributor_list:
+        
+        print(distributor)
+        # Compute distributors' Net sales
+        distributor_net_sales = compute_distributor_net_sales(distributor)
+
+        # Update distributors' Net sales
+        update_distributor_net_sales(distributor, distributor_net_sales)
+
+        print(distributor_net_sales)
+        print(distributor.net_sales)
+
+        # Compute Net profit margin
+        distributor_net_profit_margin_non_percentage = compute_distributor_net_profit_margin(distributor)
+
+        # Format value with percentage
+        distributor_net_profit_margin = format_value_with_percentage(distributor_net_profit_margin_non_percentage)
+
+        # Update distributors' Net profit margin
+        update_distributor_net_profit_margin(distributor, distributor_net_profit_margin)
 
 
-# Loop every row of CSV
-#     -if A distributors
-#         -Assign Booking, Revenue, Cost, Net sales values by modifying A distributor properties
+        print(distributor_net_profit_margin)
+        print(distributor.net_profit_margin)
 
-#     -elif B
-#         -Assign...by modifying...
-
-#     -elif C
-#         -Assign...by modifying...
-
-#     -else
-#         -Assign...by modifying...
-#             -Calculate Net profit margin
+        
